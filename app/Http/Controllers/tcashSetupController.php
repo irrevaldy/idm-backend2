e106c16ad2ee55ebe90837105367c5cfe4223850
irrevaldy <irrevaldy@gmail.com>
2018-04-10 05:08:35 +0700

    tcash check blom slesai

--- app/Http/Controllers/tcashSetupController.php
+++ app/Http/Controllers/tcashSetupController.php
@@ -19,24 +19,65 @@ class tcashSetupController extends Controller
 
   public function setTcashLimit(Request $request) {
 
-  try{
+    try{
 
-    $storeCode = $request->storeCode;
-    $currLimit = $request->currLimit;
-    $newLimit = $request->newLimit;
+      $storeCode = $request->storeCode;
+      $currLimit = $request->currLimit;
+      $newLimit = $request->newLimit;
 
-    $data = DB::statement("[spVIDM_setTcashLimit] '$storeCode', '$currLimit', '$newLimit' ");
+      $data = DB::statement("[spVIDM_setTcashLimit] '$storeCode', '$currLimit', '$newLimit' ");
 
-    $res['success'] = true;
-    $res['result'] = "Sukses";
+      $res['success'] = true;
+      $res['result'] = "Sukses";
 
-    return response($res);
-  } catch(QueryException $ex){
-    $res['success'] = false;
-    $res['result'] = 'Query Exception.. Please Check Database!';
+      return response($res);
+    } catch(QueryException $ex){
+      $res['success'] = false;
+      $res['result'] = 'Query Exception.. Please Check Database!';
 
-    return response($res);
+      return response($res);
+    }
+
   }
 
-}
+  public function checkLimit(Request $request)
+  {
+    try
+    {
+      $storeCode = $request->storeCode;
+      $branchCode = $request->branchCode;
+      $merch_id = $request->merch_id;
+
+      $data = DB::select("[spVIDM_checkLimit] '$storeCode', '$branchCode','$merch_id'");
+
+      $data = json_encode($data);
+      $data = json_decode($data, true);
+
+      $limit = $data[0]['FTCASHTOPUPLIMIT'];
+
+      if($limit == '')
+      {
+        $limit = 0;
+
+        $res['success'] = true;
+        $res['status'] = "not found";
+        $res['limit'] = $limit;
+      }
+      else
+      {
+        $res['success'] = true;
+        $res['status'] = "found";
+        $res['limit'] = $limit;
+      }
+
+      return response($res);
+    }
+    catch(QueryException $ex)
+    {
+      $res['success'] = false;
+      $res['result'] = 'Query Exception.. Please Check Database!';
+
+      return response($res);
+    }
+  }
 }
